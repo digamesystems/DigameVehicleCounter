a0abe2ae8d4128a721dc2467e20e2eb6e10f961a
John Price <john.price@digamesystems.com>
2022-08-18 16:12:34 -0700

    0.9.84: add support for changing server address in LoRa mode. Code cleanup.

--- digameVersion.h
+++ digameVersion.h
@@ -1,8 +1,8 @@
 #ifndef __DIGAME_VERSION_H__
 #define __DIGAME_VERSION_H__
 
-const String SW_VERSION       = "0.9.83";
-const String TERSE_SW_VERSION = "0983";  
+const String SW_VERSION       = "0.9.84";
+const String TERSE_SW_VERSION = "0984";  
 
 /*
  * 0.9.82 -  Added in code for Websocket server in WiFi mode. When streaming and a client is connected
@@ -12,7 +12,7 @@ const String TERSE_SW_VERSION = "0983";
  *           Moved to independent count values for each lane. 
  *           Added MAC address to LoRa message to base station.
  *           
- *           
+ * 0.9.84 -  Code cleanup, Add support for lora server parameters.           
  */
 
 #endif
